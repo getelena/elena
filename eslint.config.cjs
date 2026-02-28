6b1d89339d42a4add4031cdfb4a2626f24f527a2
Ariel Salminen <info@arielsalminen.com>
2026-02-28 05:56:10 +0200

    [chore]: Create ESLint configuration

---
+++ eslint.config.cjs
@@ -0,0 +1,50 @@
+/**
+ *  ██████████ ████
+ * ░░███░░░░░█░░███
+ *  ░███  █ ░  ░███   ██████  ████████    ██████
+ *  ░██████    ░███  ███░░███░░███░░███  ░░░░░███
+ *  ░███░░█    ░███ ░███████  ░███ ░███   ███████
+ *  ░███ ░   █ ░███ ░███░░░   ░███ ░███  ███░░███
+ *  ██████████ █████░░██████  ████ █████░░████████
+ * ░░░░░░░░░░ ░░░░░  ░░░░░░  ░░░░ ░░░░░  ░░░░░░░░
+ *
+ * Elena lint config
+ * https://elenajs.com
+ */
+const { defineConfig } = require("eslint/config");
+
+module.exports = defineConfig([
+  {
+    rules: {
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/no-require-imports": "off",
+      "comma-dangle": ["error", "only-multiline"],
+      curly: ["error", "all"],
+      "no-console": "off",
+      "no-undef": "off",
+      "no-var": "off",
+      "prefer-rest-params": "off",
+      "@typescript-eslint/no-var-requires": "off",
+      "@typescript-eslint/no-use-before-define": "off",
+      "@typescript-eslint/explicit-member-accessibility": "off",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-inferrable-types": "off",
+      "@typescript-eslint/no-this-alias": "off",
+      "@typescript-eslint/ban-ts-ignore": "off",
+    },
+  },
+  {
+    ignores: [
+      "node_modules/*",
+      "**/node_modules/*",
+      "**/dist/*",
+      "dist/*",
+      "**/dist/**/*",
+      "test/**/*",
+      "coverage/*",
+      "**/coverage/*",
+    ],
+  },
+]);
