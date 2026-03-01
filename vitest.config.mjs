2e01d27358cc77e6c84d423853dd99a0ad6c4e82
Ariel Salminen <info@arielsalminen.com>
2026-03-01 21:25:07 +0200

    [test]: Re-order project level tests

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -3,15 +3,15 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     projects: [
-      "packages/core",
-      "packages/bundler",
       "packages/cli",
+      "packages/core",
       "packages/mcp",
       "packages/ssr",
       "packages/plugin-cem-define",
       "packages/plugin-cem-tag",
       "packages/plugin-cem-typescript",
       "packages/plugin-rollup-css",
+      "packages/bundler",
     ],
   },
 });
