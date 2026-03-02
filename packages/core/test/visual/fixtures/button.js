ab0383d168c8969538fdb4251590d4b2d8493358
Ariel Salminen <info@arielsalminen.com>
2026-03-02 12:34:52 +0200

    [test]: Update visual regression tests

--- packages/core/test/visual/fixtures/button.js
+++ packages/core/test/visual/fixtures/button.js
@@ -1,20 +1,38 @@
-import { Elena, html } from "../../../src/elena.js";
+import { Elena, html, unsafeHTML, nothing } from "../../../src/elena.js";
 const options = {
   tagName: "elena-button",
-  props: ["label", "variant", "disabled", "name", "value", "type"],
+  props: [
+    "variant",
+    "size",
+    "fit-container",
+    "disabled",
+    "name",
+    "value",
+    "type",
+    { name: "icon", reflect: false },
+  ],
   events: ["click", "focus", "blur"],
 };
 export default class Button extends Elena(HTMLElement, options) {
   constructor() {
     super();
     this.variant = "default";
+    this.size = "md";
+    this.expand = false;
     this.disabled = false;
     this.name = "";
     this.value = "";
     this.type = "button";
+    this.icon = "";
   }
   render() {
-    return html`<button>${this.text}</button>`;
+    const icon = this.icon ? unsafeHTML(`<span class="elena-icon">${this.icon}</span>`) : nothing;
+    return html`
+      <button class="elena-button">
+        ${this.text ? html`<span>${this.text}</span>` : nothing}
+        ${icon}
+      </button>
+    `;
   }
 }
 Button.define();
