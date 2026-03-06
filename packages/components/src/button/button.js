edbcbe94c74288f8504cda58f00e50ed192315b5
Ariel Salminen <info@arielsalminen.com>
2026-03-06 11:38:27 +0200

    [refactor]: Transform example components to use the new static options pattern

--- packages/components/src/button/button.js
+++ packages/components/src/button/button.js
@@ -1,24 +1,5 @@
 import { Elena, html, unsafeHTML, nothing } from "@elenajs/core";
 
-const options = {
-  tagName: "elena-button",
-  props: [
-    "variant",
-    "size",
-    "expand",
-    "disabled",
-    "label",
-    "href",
-    "target",
-    "download",
-    "name",
-    "value",
-    "type",
-    { name: "icon", reflect: false },
-  ],
-  events: ["click", "focus", "blur"],
-};
-
 /**
  * Button component is used for interface actions.
  *
@@ -34,106 +15,119 @@ const options = {
  * @cssprop [--elena-button-font] - Overrides the default font family.
  * @cssprop [--elena-button-radius] - Overrides the default border radius.
  */
-export default class Button extends Elena(HTMLElement, options) {
-  constructor() {
-    super();
+export default class Button extends Elena(HTMLElement) {
+  static tagName = "elena-button";
+  static events = ["click", "focus", "blur"];
+  static props = [
+    "variant",
+    "size",
+    "expand",
+    "disabled",
+    "label",
+    "href",
+    "target",
+    "download",
+    "name",
+    "value",
+    "type",
+    { name: "icon", reflect: false },
+  ];
 
-    /**
-     * The style variant of the button.
-     *
-     * @attribute
-     * @type {"default" | "primary" | "danger" | "outline"}
-     */
-    this.variant = "default";
+  /**
+   * The style variant of the button.
+   *
+   * @attribute
+   * @type {"default" | "primary" | "danger" | "outline"}
+   */
+  variant = "default";
 
-    /**
-     * The size of the button.
-     *
-     * @attribute
-     * @type {"sm" | "md" | "lg"}
-     */
-    this.size = "md";
+  /**
+   * The size of the button.
+   *
+   * @attribute
+   * @type {"sm" | "md" | "lg"}
+   */
+  size = "md";
 
-    /**
-     * Makes the button fit its container.
-     *
-     * @attribute
-     * @type {boolean}
-     */
-    this.expand = false;
+  /**
+   * Makes the button fit its container.
+   *
+   * @attribute
+   * @type {boolean}
+   */
+  expand = false;
 
-    /**
-     * Makes the component disabled.
-     *
-     * @attribute
-     * @type {boolean}
-     */
-    this.disabled = false;
+  /**
+   * Makes the component disabled.
+   *
+   * @attribute
+   * @type {boolean}
+   */
+  disabled = false;
 
-    /**
-     * Sets aria-label for the inner button.
-     *
-     * @attribute
-     * @type {string}
-     */
-    this.label = "";
+  /**
+   * Sets aria-label for the inner button.
+   *
+   * @attribute
+   * @type {string}
+   */
+  label = "";
 
-    /**
-     * Renders the button as a link and sets a href for it.
-     *
-     * @attribute
-     * @type {string}
-     */
-    this.href = "";
+  /**
+   * Renders the button as a link and sets a href for it.
+   *
+   * @attribute
+   * @type {string}
+   */
+  href = "";
 
-    /**
-     * Defines where to open the linked URL.
-     *
-     * @attribute
-     * @type {"_self" | "_blank" | "_parent" | "_top"}
-     */
-    this.target = "_self";
+  /**
+   * Defines where to open the linked URL.
+   *
+   * @attribute
+   * @type {"_self" | "_blank" | "_parent" | "_top"}
+   */
+  target = "_self";
 
-    /**
-     * Trigger a file download instead of a page visit.
-     *
-     * @attribute
-     * @type {boolean}
-     */
-    this.download = false;
+  /**
+   * Trigger a file download instead of a page visit.
+   *
+   * @attribute
+   * @type {boolean}
+   */
+  download = false;
 
-    /**
-     * The name used to identify the button in forms.
-     *
-     * @attribute
-     * @type {string}
-     */
-    this.name = "";
+  /**
+   * The name used to identify the button in forms.
+   *
+   * @attribute
+   * @type {string}
+   */
+  name = "";
 
-    /**
-     * The value used to identify the button in forms.
-     *
-     * @attribute
-     * @type {string}
-     */
-    this.value = "";
+  /**
+   * The value used to identify the button in forms.
+   *
+   * @attribute
+   * @type {string}
+   */
+  value = "";
 
-    /**
-     * The type of the button.
-     *
-     * @attribute
-     * @type {"submit" | "reset" | "button"}
-     */
-    this.type = "button";
+  /**
+   * The type of the button.
+   *
+   * @attribute
+   * @type {"submit" | "reset" | "button"}
+   */
+  type = "button";
 
-    /**
-     * An SVG icon to display inside the button.
-     *
-     * @attribute
-     * @type {string}
-     */
-    this.icon = "";
-  }
+  /**
+   * An SVG icon to display inside the button.
+   *
+   * @attribute
+   * @type {string}
+   */
+  icon = "";
 
   /**
    * Renders a button: <button>.
