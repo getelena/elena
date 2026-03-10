60bffaa20304d2316353e0d22cdf4d232940014b
Ariel Salminen <info@arielsalminen.com>
2026-03-10 18:54:08 +0200

    [feat]: Export new components

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -1,2 +1,4 @@
 export { default as Button } from "./button/button.js";
+export { default as Spinner } from "./spinner/spinner.js";
 export { default as Stack } from "./stack/stack.js";
+export { default as VisuallyHidden } from "./visually-hidden/visually-hidden.js";
