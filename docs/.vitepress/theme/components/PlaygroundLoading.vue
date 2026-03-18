0bd422c23a548e95b62c41abca67154da53685c7
Ariel Salminen <info@arielsalminen.com>
2026-03-18 14:14:05 +0200

    [perf]: Fix load flickering in playground

--- docs/.vitepress/theme/components/PlaygroundLoading.vue
+++ docs/.vitepress/theme/components/PlaygroundLoading.vue
@@ -1,16 +1,18 @@
 <template>
-  <div class="pg-editor pg-editor-loading">
-    <div class="pg-editor-tabs">
-      <button class="pg-editor-tab active">HTML</button>
-      <button class="pg-editor-tab">CSS</button>
-      <button class="pg-editor-tab">JS</button>
+  <div class="pg-loading-skeleton">
+    <div class="pg-editor pg-editor-loading">
+      <div class="pg-editor-tabs">
+        <button class="pg-editor-tab active">HTML</button>
+        <button class="pg-editor-tab">CSS</button>
+        <button class="pg-editor-tab">JS</button>
+      </div>
+      <div class="pg-editor-container"></div>
     </div>
-    <div class="pg-editor-container"></div>
-  </div>
-  <div class="pg-preview">
-    <div class="pg-preview-header">
-      <span class="pg-preview-title">Preview</span>
+    <div class="pg-preview">
+      <div class="pg-preview-header">
+        <span class="pg-preview-title">Preview</span>
+      </div>
+      <div class="pg-preview-iframe"></div>
     </div>
-    <div class="pg-preview-iframe"></div>
   </div>
 </template>
