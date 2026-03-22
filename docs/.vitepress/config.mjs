44490e15ac96c0bb374a807b1f00ff96076a8d11
Ariel Salminen <info@arielsalminen.com>
2026-03-22 09:32:39 +0200

    [docs]: Simplify header nav

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -244,10 +244,7 @@ export default defineConfig({
       },
     ],
 
-    socialLinks: [
-      { icon: "github", link: "https://github.com/getelena/elena" },
-      { icon: "discord", link: "https://discord.gg/s2E3GtekuT" },
-    ],
+    socialLinks: [{ icon: "github", link: "https://github.com/getelena/elena" }],
 
     search: {
       provider: "local",
